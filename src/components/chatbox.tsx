1ea93ec0d804ef4fb6c5a8b4402c44e55c81bf8e
Hayotbek <idealprojectuz@gmail.com>
2025-06-21 21:31:49 +0500

    updates

--- src/components/chatbox.tsx
+++ src/components/chatbox.tsx
@@ -9,7 +9,7 @@ export const Chatbox = () => {
         className={`chatarea scrollbar-hidden pt-2  ${
           isMobile() ? "h-[87vh]" : "h-[57vh]"
         }  overflow-y-scroll  px-2 bg-[#242426] border-t border-[#444446]  `}>
-        <div className="flex justify-end mb-2">
+        <div className="flex justify-end mb-2 ">
           <div className=" rounded-[12px] text-[14px] p-2 bg-[#007AFF] border border-[#444446] text-[#F2F2F7] w-[80%]">
             Assalomu aleykum hurmatli mijoz! Hozirda barcha operatorlarimiz
             offline holatida. Savol va takliflaringizni yozib qoldiring. Biz siz
@@ -65,7 +65,7 @@ export const Chatbox = () => {
             raqamlaringizni qoldirishni unutmang.
           </div>
         </div>
-        <div className="flex justify-start mb-2">
+        <div className="flex justify-start mb-5">
           <div className=" rounded-[12px] text-[14px] p-2 bg-[#2D2D2F] border border-[#444446] text-[#F2F2F7] w-[80%]">
             Assalomu aleykum hurmatli mijoz! Hozirda barcha operatorlarimiz
             offline holatida. Savol va takliflaringizni yozib qoldiring. Biz siz
